449c2a42c8ee4edf8141923250415f0c7dc2b4a3
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2026-01-25 23:00:19 +0100

    add lecter testing

--- tests/e2e/specs/appPage.cy.js
+++ tests/e2e/specs/appPage.cy.js
@@ -22,7 +22,12 @@ describe("Testing First Page", () => {
     cy.get("div.title_head.col-sm-2.col-12")
       .should("be.visible")
       .should("contain.text", "RandomTubes");
-  })
+  });
+
+  it("Header logo", () => {
+    cy.get("div.logo_div.col-sm-2.col-12 > img").should(($img) => {
+    expect($img[0].naturalWidth).to.be.greaterThan(0);
+  })});
 
 
     /* -------------------------------------------------------------------------------------- */
@@ -56,12 +61,11 @@ describe("Testing First Page", () => {
   it("Footer is visible", () => {
     cy.get(".footer").should("be.visible");
   });
-});
 
 
-
-
-
+  /* -------------------------------------------------------------------------------------- */
+  /*                                        Testing Lecter                                  */
+  /* -------------------------------------------------------------------------------------- */
 
 it("Have an YouTube API Token", () => {
   cy.get("body").then(($body) => {
@@ -72,4 +76,53 @@ it("Have an YouTube API Token", () => {
         .should("not.contain.text", "Error : API key missing");
     }
   });
+});
+
+
+it("Initial YouTube iframe", () => {
+  cy.get("div.tube_reader > iframe")
+    .and("have.attr", "src")
+    .then((src) => {
+      expect(src).to.equal("https://www.youtube.com/embed/jNQXAC9IVRw?si=TOifG8yNjoy6pwNY");
+    });
+  });
+
+it("Prev button is disabled", () => {
+  cy.get("div > button.prev_button")
+    .should("be.visible")
+    .and("have.class", "a_button_disabled");
+});
+
+it("Next button is not disabled", () => {
+  cy.get("div > button.next_button")
+    .should("be.visible")
+    .and("not.have.class", "a_button_disabled")
+    .and("have.class", "a_button_primary");
+});
+
+it("Clicking the Next button enable the prev button and update the iframe src", () => {
+  const initialSrc =
+    "https://www.youtube.com/embed/jNQXAC9IVRw?si=TOifG8yNjoy6pwNY";
+
+  cy.get("div > button.next_button")
+    .should("be.visible")
+    .click();
+
+  cy.get("div.tube_reader > iframe")
+    .should("have.attr", "src")
+    .and("not.equal", initialSrc);
+
+  cy.get("div > button.prev_button")
+    .should("be.visible")
+    .and("not.have.class", "a_button_disabled")
+    .click();
+
+  cy.get("div.tube_reader > iframe")
+    .should("have.attr", "src")
+    .and("equal", initialSrc);
+});
+
+
+
+
 });
